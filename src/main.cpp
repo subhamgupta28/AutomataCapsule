eea81c7ff7319e8c5555c2079bee5b74dd023c7a
Subham gupta <67224573+subhamgupta28@users.noreply.github.com>
2025-11-16 22:49:06 +0530

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <Adafruit_AHTX0.h>
 #include <Wire.h>
 #include <BH1750.h>
-#include "RadarSensor.h"
+// #include "RadarSensor.h"
 #define I2C_SDA_PIN 14 // Define your custom SDA pin
 #define I2C_SCL_PIN 13
 #define BAUD_RATE 256000
@@ -20,7 +20,7 @@ int MQTT_PORT = 1883;
 BH1750 lightMeter;
 Automata automata("Presence", HOST, PORT, MQTT_HOST, MQTT_PORT);
 // SoftwareSerial mySerial(3, 2);
-RadarSensor radar(Serial1);
+// RadarSensor radar(Serial1);
 
 Adafruit_AHTX0 aht;
 // Variables
@@ -57,7 +57,7 @@ String str;
 String motion;
 JsonDocument doc;
 bool light = false;
-RadarTarget currentTarget;
+// RadarTarget currentTarget;
 bool targetDetected = false;
 
 void action(const Action action)
@@ -79,105 +79,105 @@ void sendData()
     automata.sendData(doc);
 }
 // Function to print buffer contents
-void printBuffer()
-{
-    Serial.print("RX_BUF: ");
-    for (int i = 0; i < RX_count; i++)
-    {
-        Serial.print("0x");
-        if (RX_BUF[i] < 0x10)
-            Serial.print("0"); // Add leading zero for single-digit hex values
-        Serial.print(RX_BUF[i], HEX);
-        Serial.print(" ");
-    }
-    Serial.println();
-}
-void processRadarData()
-{
+// void printBuffer()
+// {
+//     Serial.print("RX_BUF: ");
+//     for (int i = 0; i < RX_count; i++)
+//     {
+//         Serial.print("0x");
+//         if (RX_BUF[i] < 0x10)
+//             Serial.print("0"); // Add leading zero for single-digit hex values
+//         Serial.print(RX_BUF[i], HEX);
+//         Serial.print(" ");
+//     }
+//     Serial.println();
+// }
+// void processRadarData()
+// {
 
-    // output data
-    // printBuffer();
+//     // output data
+//     // printBuffer();
 
-    /* RX_BUF: 0xAA 0xFF 0x03 0x00                   Header
-     *  0x05 0x01 0x19 0x82 0x00 0x00 0x68 0x01      target1
-     *  0xE3 0x81 0x33 0x88 0x20 0x80 0x68 0x01      target2
-     *  0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00      target3
-     *  0x55 0xCC
-     */
+//     /* RX_BUF: 0xAA 0xFF 0x03 0x00                   Header
+//      *  0x05 0x01 0x19 0x82 0x00 0x00 0x68 0x01      target1
+//      *  0xE3 0x81 0x33 0x88 0x20 0x80 0x68 0x01      target2
+//      *  0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00      target3
+//      *  0x55 0xCC
+//      */
 
-    if (RX_count >= 32)
-    {
-        // Extract data for Target 1
-        target1_x = (RX_BUF[4] | (RX_BUF[5] << 8)) - 0x200;
-        target1_y = (RX_BUF[6] | (RX_BUF[7] << 8)) - 0x8000;
-        target1_speed = (RX_BUF[8] | (RX_BUF[9] << 8)) - 0x10;
-        target1_distance_res = (RX_BUF[10] | (RX_BUF[11] << 8));
-        target1_distance = sqrt(pow(target1_x, 2) + pow(target1_y, 2)) / 10.0;
-        target1_angle = atan2(target1_y, target1_x) * 180.0 / PI;
+//     if (RX_count >= 32)
+//     {
+//         // Extract data for Target 1
+//         target1_x = (RX_BUF[4] | (RX_BUF[5] << 8)) - 0x200;
+//         target1_y = (RX_BUF[6] | (RX_BUF[7] << 8)) - 0x8000;
+//         target1_speed = (RX_BUF[8] | (RX_BUF[9] << 8)) - 0x10;
+//         target1_distance_res = (RX_BUF[10] | (RX_BUF[11] << 8));
+//         target1_distance = sqrt(pow(target1_x, 2) + pow(target1_y, 2)) / 10.0;
+//         target1_angle = atan2(target1_y, target1_x) * 180.0 / PI;
 
-        // Serial.print("Target 1 - Distance: ");
-        // Serial.print(target1_distance);
-        // Serial.print(" cm, Angle: ");
-        // Serial.print(target1_angle);
-        // Serial.print(" degrees, X: ");
-        // Serial.print(target1_x);
-        // Serial.print(" mm, Y: ");
-        // Serial.print(target1_y);
-        // Serial.print(" mm, Speed: ");
-        // Serial.print(target1_speed);
-        // Serial.print(" cm/s, Distance Resolution: ");
-        // Serial.print(target1_distance_res);
-        // Serial.println(" mm");
+//         // Serial.print("Target 1 - Distance: ");
+//         // Serial.print(target1_distance);
+//         // Serial.print(" cm, Angle: ");
+//         // Serial.print(target1_angle);
+//         // Serial.print(" degrees, X: ");
+//         // Serial.print(target1_x);
+//         // Serial.print(" mm, Y: ");
+//         // Serial.print(target1_y);
+//         // Serial.print(" mm, Speed: ");
+//         // Serial.print(target1_speed);
+//         // Serial.print(" cm/s, Distance Resolution: ");
+//         // Serial.print(target1_distance_res);
+//         // Serial.println(" mm");
 
-        // Extract data for Target 2
-        target2_x = (RX_BUF[12] | (RX_BUF[13] << 8)) - 0x200;
-        target2_y = (RX_BUF[14] | (RX_BUF[15] << 8)) - 0x8000;
-        target2_speed = (RX_BUF[16] | (RX_BUF[17] << 8)) - 0x10;
-        target2_distance_res = (RX_BUF[18] | (RX_BUF[19] << 8));
-        target2_distance = sqrt(pow(target2_x, 2) + pow(target2_y, 2)) / 10.0;
-        target2_angle = atan2(target2_y, target2_x) * 180.0 / PI;
+//         // Extract data for Target 2
+//         target2_x = (RX_BUF[12] | (RX_BUF[13] << 8)) - 0x200;
+//         target2_y = (RX_BUF[14] | (RX_BUF[15] << 8)) - 0x8000;
+//         target2_speed = (RX_BUF[16] | (RX_BUF[17] << 8)) - 0x10;
+//         target2_distance_res = (RX_BUF[18] | (RX_BUF[19] << 8));
+//         target2_distance = sqrt(pow(target2_x, 2) + pow(target2_y, 2)) / 10.0;
+//         target2_angle = atan2(target2_y, target2_x) * 180.0 / PI;
 
-        // Serial.print("Target 2 - Distance: ");
-        // Serial.print(target2_distance );
-        // Serial.print(" cm, Angle: ");
-        // Serial.print(target2_angle);
-        // Serial.print(" degrees, X: ");
-        // Serial.print(target2_x);
-        // Serial.print(" mm, Y: ");
-        // Serial.print(target2_y);
-        // Serial.print(" mm, Speed: ");
-        // Serial.print(target2_speed);
-        // Serial.print(" cm/s, Distance Resolution: ");
-        // Serial.print(target2_distance_res);
-        // Serial.println(" mm");
+//         // Serial.print("Target 2 - Distance: ");
+//         // Serial.print(target2_distance );
+//         // Serial.print(" cm, Angle: ");
+//         // Serial.print(target2_angle);
+//         // Serial.print(" degrees, X: ");
+//         // Serial.print(target2_x);
+//         // Serial.print(" mm, Y: ");
+//         // Serial.print(target2_y);
+//         // Serial.print(" mm, Speed: ");
+//         // Serial.print(target2_speed);
+//         // Serial.print(" cm/s, Distance Resolution: ");
+//         // Serial.print(target2_distance_res);
+//         // Serial.println(" mm");
 
-        // Extract data for Target 3
-        target3_x = (RX_BUF[20] | (RX_BUF[21] << 8)) - 0x200;
-        target3_y = (RX_BUF[22] | (RX_BUF[23] << 8)) - 0x8000;
-        target3_speed = (RX_BUF[24] | (RX_BUF[25] << 8)) - 0x10;
-        target3_distance_res = (RX_BUF[26] | (RX_BUF[27] << 8));
-        target3_distance = sqrt(pow(target3_x, 2) + pow(target3_y, 2)) / 10.0;
-        target3_angle = atan2(target3_y, target3_x) * 180.0 / PI;
+//         // Extract data for Target 3
+//         target3_x = (RX_BUF[20] | (RX_BUF[21] << 8)) - 0x200;
+//         target3_y = (RX_BUF[22] | (RX_BUF[23] << 8)) - 0x8000;
+//         target3_speed = (RX_BUF[24] | (RX_BUF[25] << 8)) - 0x10;
+//         target3_distance_res = (RX_BUF[26] | (RX_BUF[27] << 8));
+//         target3_distance = sqrt(pow(target3_x, 2) + pow(target3_y, 2)) / 10.0;
+//         target3_angle = atan2(target3_y, target3_x) * 180.0 / PI;
 
-        // Serial.print("Target 3 - Distance: ");
-        // Serial.print(target3_distance);
-        // Serial.print(" cm, Angle: ");
-        // Serial.print(target3_angle);
-        // Serial.print(" degrees, X: ");
-        // Serial.print(target3_x);
-        // Serial.print(" mm, Y: ");
-        // Serial.print(target3_y);
-        // Serial.print(" mm, Speed: ");
-        // Serial.print(target3_speed);
-        // Serial.print(" cm/s, Distance Resolution: ");
-        // Serial.print(target3_distance_res);
-        // Serial.println(" mm");
+//         // Serial.print("Target 3 - Distance: ");
+//         // Serial.print(target3_distance);
+//         // Serial.print(" cm, Angle: ");
+//         // Serial.print(target3_angle);
+//         // Serial.print(" degrees, X: ");
+//         // Serial.print(target3_x);
+//         // Serial.print(" mm, Y: ");
+//         // Serial.print(target3_y);
+//         // Serial.print(" mm, Speed: ");
+//         // Serial.print(target3_speed);
+//         // Serial.print(" cm/s, Distance Resolution: ");
+//         // Serial.print(target3_distance_res);
+//         // Serial.println(" mm");
 
-        // Reset buffer and counter
-        memset(RX_BUF, 0x00, sizeof(RX_BUF));
-        RX_count = 0;
-    }
-}
+//         // Reset buffer and counter
+//         memset(RX_BUF, 0x00, sizeof(RX_BUF));
+//         RX_count = 0;
+//     }
+// }
 
 void setup()
 {
@@ -190,7 +190,7 @@ void setup()
     pinMode(BUTTON1, INPUT);
     pinMode(BUTTON2, INPUT);
     Serial1.begin(BAUD_RATE, SERIAL_8N1, 3, 2);
-    radar.begin();
+    // radar.begin();
     // Serial1.setRxBufferSize(64);
     // Serial1.write(Multi_Target_Detection_CMD, sizeof(Multi_Target_Detection_CMD));
     Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
@@ -211,10 +211,10 @@ void setup()
     JsonDocument doc;
     doc["max"] = 100;
     doc["min"] = 0;
-    automata.addAttribute("x", "X", "MM", "DATA|RADAR");
-    automata.addAttribute("y", "Y", "MM", "DATA|RADAR");
-    automata.addAttribute("distance", "T1 Distance", "CM", "DATA|RADAR");
-    automata.addAttribute("angle", "T1 Angle", "Deg", "DATA|RADAR");
+    // automata.addAttribute("x", "X", "MM", "DATA|RADAR");
+    // automata.addAttribute("y", "Y", "MM", "DATA|RADAR");
+    // automata.addAttribute("distance", "T1 Distance", "CM", "DATA|RADAR");
+    // automata.addAttribute("angle", "T1 Angle", "Deg", "DATA|RADAR");
 
     // automata.addAttribute("target2_x", "T2 X", "MM", "DATA|AUX");
     // automata.addAttribute("target2_y", "T2 Y", "MM", "DATA|AUX");
@@ -346,7 +346,7 @@ void readSensor()
         // Check for end of frame (0xCC, 0x55)
         if ((RX_count > 1) && (RX_BUF[RX_count - 1] == 0xCC) && (RX_BUF[RX_count - 2] == 0x55))
         {
-            processRadarData();
+            // processRadarData();
         }
     }
 }
@@ -354,24 +354,24 @@ void loop()
 {
 
     // readSensor();
-    if (radar.update())
-    {
-        currentTarget = radar.getTarget();
-        doc["distance"] = currentTarget.distance;
-        doc["x"] = currentTarget.x;
-        doc["y"] = currentTarget.y;
-        doc["angle"] = currentTarget.angle;
-        Serial.print("Distance: ");
-        Serial.print(currentTarget.distance);
-        Serial.print("mm, Angle: ");
-        Serial.print(currentTarget.angle);
-        Serial.print("°, X: ");
-        Serial.print(currentTarget.x);
-        Serial.print("mm, Y: ");
-        Serial.print(currentTarget.y);
-        Serial.println("mm");
-        // Serial.println(targetDetected ? "YES" : "NO");
-    }
+    // if (radar.update())
+    // {
+    //     currentTarget = radar.getTarget();
+    //     doc["distance"] = currentTarget.distance;
+    //     doc["x"] = currentTarget.x;
+    //     doc["y"] = currentTarget.y;
+    //     doc["angle"] = currentTarget.angle;
+    //     Serial.print("Distance: ");
+    //     Serial.print(currentTarget.distance);
+    //     Serial.print("mm, Angle: ");
+    //     Serial.print(currentTarget.angle);
+    //     Serial.print("°, X: ");
+    //     Serial.print(currentTarget.x);
+    //     Serial.print("mm, Y: ");
+    //     Serial.print(currentTarget.y);
+    //     Serial.println("mm");
+    //     // Serial.println(targetDetected ? "YES" : "NO");
+    // }
     // str = readSerialData();
     sensors_event_t humidity, temp;
     aht.getEvent(&humidity, &temp);
